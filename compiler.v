ebf038df04a8594d5f2fd5df1ebf1f3f5de8909d
Nicolas Iooss <nicolas.iooss_git@polytechnique.org>
2013-03-01 17:01:05 +0100

    Redefine Krivine state to do Part 5
    Signed-off-by: Nicolas Iooss <nicolas.iooss_git@polytechnique.org>

--- compiler.v
+++ compiler.v
@@ -1,5 +1,5 @@
 (** 4. Compiler *)
-Require Import krivine List lterm substitute_list.
+Require Import free_variables krivine List lterm substitute_list.
 
 Fixpoint comp (t: lterm) : krivine_code :=
   match t with
@@ -37,7 +37,7 @@ Fixpoint tau_env (e: krivine_env) : list lterm :=
 .
 
 (** Reverse translate a Krivine state,
- * using a ecursive routine which works with an accumulator
+    using a Recursive routine which works with an accumulator
  *)
 Fixpoint list_lterm_to_apply (a: option lterm) (l: list lterm) : option lterm :=
   match l, a with
@@ -46,19 +46,45 @@ Fixpoint list_lterm_to_apply (a: option lterm) (l: list lterm) : option lterm :=
     | u :: l0, Some t => list_lterm_to_apply (Some (Apply t u)) l0
   end
 .
-Definition tau_state (s: krivine_state) : option lterm :=
-  match s with
-    | KState c e e' => list_lterm_to_apply None (tau_env (KEnv c e e'))
-  end
+Definition tau_state (st: krivine_env) : option lterm :=
+  list_lterm_to_apply None (tau_env st)
 .
 
+(** To define correct state, define C[n] predicate ("fr_below" in code) on a code *)
+Definition closed_code (n: nat) (c: krivine_code): Prop :=
+   exists t: lterm, tau_code c = Some t /\ fr_below n t
+.
+
+(** Correct environment/stack/state (that's the same object) *)
+Inductive correct_env: krivine_env -> Prop :=
+  | Correct_env_nil: correct_env KEnv_nil
+  | Correct_env:
+    forall (c0: krivine_code) (e0 e: krivine_env),
+    closed_code (klength e0) c0 -> correct_env e0 -> correct_env e ->
+    correct_env (KEnv c0 e0 e)
+.
+
+(** A correct state is a correct non-empty environment *)
+Definition correct_state (st: krivine_env) : Prop :=
+  st <> KEnv_nil /\ correct_env st
+.
+
+Lemma correct_state_nonnil:
+  forall (c0: krivine_code) (e0 e: krivine_env),
+  correct_env (KEnv c0 e0 e) -> correct_state (KEnv c0 e0 e).
+Proof.
+  intros c0 e0 e H.
+  unfold correct_state.
+  apply conj; trivial.
+  intro H0. inversion H0.
+Save.
+
 (** Proofs *)
-Lemma compile_term_tau_ident:
+(** 5.2. *)
+Theorem compile_term_tau_ident:
   forall t: lterm, tau_code (comp t) = Some t.
 Proof.
   induction t; simpl; trivial.
   rewrite IHt; trivial.
   rewrite IHt1; rewrite IHt2; trivial.
 Save.
-
-(** TODO: right state *)
--- krivine.v
+++ krivine.v
@@ -20,19 +20,30 @@ Inductive krivine_env : Set :=
   | KEnv: krivine_code -> krivine_env -> krivine_env -> krivine_env
 .
 
-(* State *)
-Inductive krivine_state : Set :=
-  | KState: krivine_code -> krivine_env -> krivine_env -> krivine_state.
+(** a state is (code, env, stack) tuple which may be seen as an environment *)
+Definition krivine_state (c: krivine_code) (e s: krivine_env) : krivine_env :=
+  KEnv c e s
+.
 
-(** 3.3. Semantic: execute one step *)
-Definition krivine_step (st: krivine_state) : option krivine_state :=
+(** Length of an environment list *)
+Fixpoint klength (e: krivine_env) : nat :=
+  match e with
+    | KEnv_nil => O
+    | KEnv _ _ e0 => S (klength e0)
+  end
+.
+
+(** 3.3. Semantic: execute one step.
+  Return KEnv_nil if there were some error, a krivine_state otherwise.
+ *)
+Definition krivine_step (st: krivine_env) : krivine_env :=
   match st with
-    | KState (Access O _) (KEnv c0 e0 _) s => Some (KState c0 e0 s)
-    | KState (Access (S n) c) (KEnv _ _ e) s =>
-        Some (KState (Access n c) e s)
-    | KState (Push c0 c) e s => Some (KState c e (KEnv c0 e s))
-    | KState (Grab c) e (KEnv c0 e0 s) =>
-        Some (KState c (KEnv c0 e0 e) s)
-    | _ => None
+    | KEnv (Access O _) (KEnv c0 e0 _) s => krivine_state c0 e0 s
+    | KEnv (Access (S n) c) (KEnv _ _ e) s =>
+        krivine_state (Access n c) e s
+    | KEnv (Push c0 c) e s => krivine_state c e (KEnv c0 e s)
+    | KEnv (Grab c) e (KEnv c0 e0 s) =>
+        krivine_state c (KEnv c0 e0 e) s
+    | _ => KEnv_nil
   end
 .
