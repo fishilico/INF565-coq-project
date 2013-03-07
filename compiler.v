7b68c2446b0d01acfb606691672839b24912c830
Nicolas Iooss <nicolas.iooss_git@polytechnique.org>
2013-03-07 10:54:54 +0100

    Begin 5.2. proof
    Signed-off-by: Nicolas Iooss <nicolas.iooss_git@polytechnique.org>

--- compiler.v
+++ compiler.v
@@ -69,16 +69,6 @@ Definition correct_state (st: krivine_env) : Prop :=
   st <> KEnv_nil /\ correct_env st
 .
 
-Lemma correct_state_nonnil:
-  forall (c0: krivine_code) (e0 e: krivine_env),
-  correct_env (KEnv c0 e0 e) -> correct_state (KEnv c0 e0 e).
-Proof.
-  intros c0 e0 e H.
-  unfold correct_state.
-  apply conj; trivial.
-  intro H0. inversion H0.
-Save.
-
 (** Proofs *)
 (** 5.2. *)
 Theorem compile_term_tau_ident:
@@ -88,3 +78,78 @@ Proof.
   rewrite IHt; trivial.
   rewrite IHt1; rewrite IHt2; trivial.
 Save.
+
+(** 5.3. *)
+Lemma correct_env_is_state:
+  forall (c0: krivine_code) (e0 e: krivine_env),
+  correct_env (KEnv c0 e0 e) -> correct_state (KEnv c0 e0 e).
+Proof.
+  intros c0 e0 e H.
+  unfold correct_state.
+  apply conj; trivial.
+  intro H0. inversion H0.
+Save.
+
+(** An environment with Nop as code is always incorrect *)
+Lemma not_closed_Nop:
+  forall n: nat, ~ closed_code n Nop.
+Proof.
+  intros n H.
+  inversion H.
+  destruct H0.
+  contradict H0.
+  simpl.
+  intro H0.
+  inversion H0.
+Save.
+Lemma incorrect_Nop:
+  forall (e0 e: krivine_env), ~ correct_env (KEnv Nop e0 e).
+Proof.
+  intros e0 e H.
+  inversion H.
+  contradict H3.
+  apply not_closed_Nop.
+Save.
+
+(** A state with Access 0 needs a non-empty environment *)
+Lemma not_closed_Access:
+  forall (n: nat) (c: krivine_code), ~ closed_code O (Access n c).
+Proof.
+  intros n c H.
+  inversion H.
+  contradict H0.
+  simpl.
+  intro H0.
+  destruct H0.
+  inversion H0.
+  contradict H1.
+  rewrite <- H3.
+  simpl.
+  auto with arith.
+Save.
+
+Lemma incorrect_Access:
+  forall (n: nat) (c: krivine_code) (s: krivine_env),
+  ~ correct_env (KEnv (Access n c) KEnv_nil s).
+Proof.
+  intros n c s H.
+  inversion H.
+  contradict H3; simpl.
+  apply not_closed_Access.
+Save.
+(*
+Theorem step_correctness:
+  forall st: krivine_env, correct_state st -> correct_state (krivine_step st).
+Proof.
+  induction st; trivial.
+  induction k; trivial; intro H.
+
+  (* State: Nop st1 st2 *)
+  inversion H.
+  contradict H1.
+  apply incorrect_Nop.
+
+  (* State: (Access n k) st1 st2 *)
+  inversion H.
+(* Not ended *)
+*)
